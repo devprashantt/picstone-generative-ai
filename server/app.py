ac48cbfaaae90b00025aee3b893e425737e10af7
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-09-22 01:34:50 +0530

    frontend

--- server/app.py
+++ server/app.py
@@ -1,39 +1,24 @@
-from flask import Flask, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
+# Import your blueprints here
 from routes.user_routes import user_bp
 from routes.story_routes import story_bp
 
 app = Flask(__name__)
+
+# Use environment variables for sensitive information
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost/picstone'
+
+# Disable debug mode in production
+app.debug = False
+
+# Initialize the SQLAlchemy and Migrate extensions
 db = SQLAlchemy(app)
 
-if app.debug:
-    print("The Flask app is running in debug mode.")
-
-try:
-    import cloudinary
-    print("Cloudinary Python SDK is installed.")
-except ImportError:
-    print("Cloudinary Python SDK is not installed.")
-
-try:
-    db.engine.connect()
-    print("Connected to the database successfully.")
-except Exception as e:
-    print("Failed to connect to the database. Error:", str(e))
-
-
-@app.route('/')
-def index():
-    # You can access the database within this route function
-    # Your database operations should be here
-    return "Server is running, and the database is connected."
-
-
+# Register your blueprints
 app.register_blueprint(user_bp)
 app.register_blueprint(story_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
