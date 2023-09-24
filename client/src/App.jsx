f281122be2a5d58066dcb787d0eb92be14d93c77
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-09-25 00:54:07 +0530

    img uploader completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,14 @@
 import { Landing } from "./pages";
 
+import axios from "axios";
+
 const App = () => {
+  // SET BASE URL
+  axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+  // SET CONTENT TYPE
+  axios.defaults.headers.post["Content-Type"] = "application/json";
+
   return (
     <div>
       <Landing />
--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,7 +1,18 @@
+// REACT IMPORTS
+import { Link } from "react-router-dom";
+
+// CONSTANTS
 import styles from "./Card.module.scss";
 
 const Card = () => {
-  return <div className={styles.card}>Card</div>;
+  return (
+    <div className={styles.card}>
+      <img src="" alt="" className={styles.img} />
+      <h3 className={styles.heading}></h3>
+      <p className={styles.description}></p>
+      <Link className={styles.link}></Link>
+    </div>
+  );
 };
 
 export default Card;
