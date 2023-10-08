09967d3095efbfe5729055aa99384ca09748635d
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-10-08 13:53:52 +0530

    img-analysis

--- server/utils/image_analysis.py
+++ server/utils/image_analysis.py
@@ -2,13 +2,3 @@ import pytesseract
 import cv2
 import matplotlib.pyplot as plt
 from PIL import Image
-
-# read the image using OpenCV
-image = cv2.imread("generative-ai.png")
-# or you can use Pillow
-# image = Image.open("test.png")
-
-# get the string
-string = pytesseract.image_to_string(image)
-# print it
-print(string)
