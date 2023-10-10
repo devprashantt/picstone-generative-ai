4514da1ebeff06189b66c12cb6e87be37c36cabd
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-10-10 18:37:53 +0530

    working on img

--- server/app.py
+++ server/app.py
@@ -13,8 +13,6 @@ from config.database import db, database_url
 from config.cloudinary import cloudinary
 from config.open_ai import openai
 
-from utils.image_analysis import string
-
 import os
 from dotenv import load_dotenv
 
