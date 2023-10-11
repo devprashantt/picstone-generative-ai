f5de5ecbeabc5375a2a8607d41a451e27d83c671
aidancanavan <aidan.canavan3@gmail.com>
2023-10-10 21:11:36 -0400

    finished forgot and update password routes

--- server/controllers/user_controller.py
+++ server/controllers/user_controller.py
@@ -5,6 +5,7 @@ from models.user import User
 import uuid
 import bcrypt
 import os
+import hashlib
 from utils import session_tools
 
 
@@ -66,8 +67,19 @@ class UserController:
         except Exception as e:
             return f'invalid: {e}', 400
         
+    def is_user_verified(email):
+        query = 'SELECT verification_id from user where email = %s;'
+        data = (email,)
+        values = db.engine.execute(query, data)
+        row = values.fetchone()
+        if row.verification_id == 'verified':
+            return True
+        else:
+            return False
+        
     @classmethod
     def verfiy_user(cls):
+        #TODO This route is timing attack vulnerable FIX -> hash the verification token
         verification_token = request.args.get('token',None,str)
         if not verification_token:
             return 'could not verify user', 400
@@ -88,12 +100,51 @@ class UserController:
         except:
             return 'invalid token', 400
 
-    def is_user_verified(email):
-        query = 'SELECT verification_id from user where email = %s;'
-        data = (email,)
-        values = db.engine.execute(query, data)
-        row = values.fetchone()
-        if row.verification_id == 'verified':
-            return True
-        else:
-            return False
+
+    
+    @classmethod
+    def forgot_password(cls):
+        payload = request.get_json()
+        email = payload.get('email')
+        forgot_password_key = str(uuid.uuid4())
+        hasher = hashlib.sha256()
+        hasher.update(forgot_password_key.encode('utf-8'))
+        forgot_password_hash = hasher.hexdigest()
+        try:
+            query =  "UPDATE user SET forgot_password_token = %s WHERE email = %s;"
+            db.engine.execute(query,(forgot_password_hash,email))
+        except:
+            return 'connection error', 400
+        try:
+            # TODO URL needs to point to a frontend route that can take query parameter
+            parameterized_url = f"{os.environ.get('FRONTEND_URL')}/updatepassword?token={forgot_password_key}"
+            email_content = render_template('verification_email.html', path=parameterized_url )
+            msg = Message("Picstone: Forgot Password",
+                        sender="picstoneai@gmail.com",
+                        recipients=[email])
+            msg.html = email_content
+            current_app.mail.send(msg)
+            return "email sent to inbox", 200
+        except:
+            return 'email invalid, try again', 400
+
+    @classmethod
+    def update_password(cls):
+        token = request.args.get('token',None, str)
+        if not token:
+            return 'no token provided', 400
+        hasher = hashlib.sha256()
+        hasher.update(token.encode('utf-8'))
+        token_hash =  hasher.hexdigest()
+        payload = request.get_json()
+        new_password = payload.get('password')
+        if not new_password:
+            return 'no new password supplied'
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), salt)
+        try:
+            query = "UPDATE user SET password_hash = %s, salt = %s, forgot_password_token = NULL WHERE forgot_password_token = %s;"
+            db.engine.execute(query,(hashed_password, salt, token_hash))
+            return 'password updated',200
+        except:
+            'could not update password', 400
