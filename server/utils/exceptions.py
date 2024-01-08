9ecf9a49b2c926952fe082d58b13c96a68dd6e08
Rish <70828054+Kyroyen@users.noreply.github.com>
2024-01-08 14:43:55 +0530

    possibly final revert
    This reverts commit 31528e60b8562801e21f3d3d13f8dce0a935403e.

--- server/utils/exceptions.py
+++ server/utils/exceptions.py
@@ -1,6 +1,5 @@
-from app import app
-
-SHOW_INTERNAL_ERRORS = app.debug
+from constants.constant_data import DEBUG
+SHOW_INTERNAL_ERRORS = DEBUG
 
 class BAD_REQUEST_EXCEPTION(Exception):
     def __init__(self, message:str) -> None:
@@ -9,5 +8,10 @@ class BAD_REQUEST_EXCEPTION(Exception):
 
 class INTERNAL_SERVER_ERROR_EXCEPTION(Exception):
     def __init__(self, message:str) -> None:
-        self.message = "INTERNAL SERVER ERROR"+ message if SHOW_INTERNAL_ERRORS else ""
+        self.message = "INTERNAL SERVER ERROR : "+ message if SHOW_INTERNAL_ERRORS else ""
         self.error = 500
+
+class NOT_FOUND_EXCEPTION_404(Exception):
+    def __init__(self, message:str) -> None:
+        self.message = f"NOT FOUND : {message}"
+        self.error = 404
--- server/utils/story_utils.py
+++ server/utils/story_utils.py
@@ -11,12 +11,13 @@ from config.database import db
 # UTILS
 from utils.generate_story import generate_story
 from utils.upload_img import upload_image_to_cloudinary
-from utils.exceptions import INTERNAL_SERVER_ERROR_EXCEPTION,BAD_REQUEST_EXCEPTION
+from utils.exceptions import INTERNAL_SERVER_ERROR_EXCEPTION, BAD_REQUEST_EXCEPTION
 
 # MODELS
 from models.story import Story
 from models.image import Image
 from models.tags import Tags
+
 
 def extract_payload_data(payload):
     try:
@@ -29,18 +30,20 @@ def extract_payload_data(payload):
         themes = payload.get('themes', CONSTANTS.DEFAULT_THEMES)
         genre = payload.get("genre", CONSTANTS.DEFAULT_GENRE)
         return {
-            "file" : file,
-            "themes" : themes,
-            "selected_themes" : selected_themes,
-            "desc" : desc,
-            "title" : title,
-            "user_email" : user_email,
-            "themes" : themes,
-            "genre" : genre,
+            "file": file,
+            "themes": themes,
+            "selected_themes": selected_themes,
+            "description": desc,
+            "title": title,
+            "user_email": user_email,
+            "themes": themes,
+            "genre": genre,
         }
     except Exception:
-        raise BAD_REQUEST_EXCEPTION("One or More parameters missing in request")
-    
+        raise BAD_REQUEST_EXCEPTION(
+            "One or More parameters missing in request")
+
+
 def get_user_info(session_token, email):
     try:
         if session_token:
@@ -50,36 +53,41 @@ def get_user_info(session_token, email):
                 "SELECT id FROM users WHERE email = %s;", (user_email,)).fetchone().id
         else:
             user_email = email
-            user_id = DEFAULT_USER_ID  # Set a default user id when not logged in
+            user_id = CONSTANTS.DEFAULT_USER_ID_STORY  # Set a default user id when not logged in
 
         return user_id, user_email
     except Exception:
         raise INTERNAL_SERVER_ERROR_EXCEPTION("Can't Fetch User Mail or ID")
 
 
-
 def extract_ai_content(cloudinary_data):
     try:
         ai_content = ""
         if cloudinary_data["info"]["detection"]["captioning"]["status"] == "complete":
-            ai_content = cloudinary_data["info"]["detection"]["captioning"]["data"]["caption"]
+            ai_content = cloudinary_data["info"]["detection"]["captioning"].get(
+                "data", {"caption": CONSTANTS.DEFAULT_CAPTION})["caption"]
         return ai_content
     except Exception:
-        raise INTERNAL_SERVER_ERROR_EXCEPTION("Error in Extracting Coludinary Data : AI_CONTENT")
+        raise INTERNAL_SERVER_ERROR_EXCEPTION(
+            "Error in Extracting Coludinary Data : AI_CONTENT")
+
 
 def extract_text_data(cloudinary_data):
     try:
         image_text = ""
         if cloudinary_data["info"]["ocr"]["adv_ocr"]["status"] == "complete":
-            image_text = cloudinary_data["info"]["ocr"]["adv_ocr"]["data"][0]["fullTextAnnotation"]["text"]
+            # print(cloudinary_data["info"]["ocr"]["adv_ocr"]["status"])
+            image_text = cloudinary_data["info"]["ocr"]["adv_ocr"]["data"][0].get(
+                "fullTextAnnotation", {"text": CONSTANTS.DEFAULT_OCR})["text"]
         return image_text
-    except Exception:
-        raise INTERNAL_SERVER_ERROR_EXCEPTION("Error in Extracting Coludinary Data : OCR_DATA")
+    except Exception as e:
+        raise INTERNAL_SERVER_ERROR_EXCEPTION(
+            f"Error in Extracting Coludinary Data : OCR_DATA({e.args})")
 
 
-def check_keywords(title:str, genre:str, desc:str, tags:List[str]):
+def check_keywords(title: str, genre: str, desc: str, tags: List[str]):
     error = []
-
+    
     if title.lower() in CONSTANTS.KEYWORDS_TO_CHECK:
         error.append("title")
 
@@ -93,57 +101,39 @@ def check_keywords(title:str, genre:str, desc:str, tags:List[str]):
         error.append("genre")
 
     if error:
-        raise BAD_REQUEST_EXCEPTION("Can't generate using these " + ", ".join(error))
+        raise BAD_REQUEST_EXCEPTION(
+            "Can't generate using these " + ", ".join(error))
 
-
-def generate_and_save_story(user_id, user_email, image_text, title, ai_content, desc, selected_themes, cloudinary_data, tags, tag_analysis):
-    tags_string = ','.join(cloudinary_data['tags'])
-
-    story = generate_story(tags, tag_analysis, image_text,
-                           title, ai_content, desc, selected_themes)
-
-    new_image = Image(
-        user_id=user_id,
-        image_path=cloudinary_data['secure_url'],
-    )
+def save_image(new_image:Image):
+    # Save the image in the "images" table
 
     try:
         db.session.add(new_image)
         db.session.commit()
+        return new_image
     except Exception as e:
         db.session.rollback()
-        raise e
+        raise INTERNAL_SERVER_ERROR_EXCEPTION(
+            f'error during saving image in database : {e.args}')
+        # return jsonify({'error during saving image in database': str(e)}), 500
 
-    image_id = new_image.id
-
-    new_story = Story(
-        user_id=user_id,
-        user_email=user_email,
-        image_id=image_id,
-        story_content=story,
-        story_title=title,
-        ai_content=ai_content,
-        theme=','.join(selected_themes)
-    )
-
+def save_story(new_story: Story):
     try:
+        # Add the new story to the database
         db.session.add(new_story)
         db.session.commit()
     except Exception as e:
         db.session.rollback()
-        raise e
+        raise INTERNAL_SERVER_ERROR_EXCEPTION(
+            message=f'error during saving story on database : {e.args}',
+        )
 
-    story_id = new_story.id
-
-    new_tags = Tags(
-        story_id=story_id,
-        image_id=image_id,
-        tags_string=tags_string
-    )
-
+def save_tags(new_tags:Tags):
     try:
         db.session.add(new_tags)
         db.session.commit()
     except Exception as e:
         db.session.rollback()
-        raise e
+        raise INTERNAL_SERVER_ERROR_EXCEPTION(
+            message=f'error during saving tags on database : {e.args}',
+        )
